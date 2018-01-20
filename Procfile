a057ccdefb940508eddc80a0a273506d51b78ee3
ecogsdill <emily.cogsdill@gmail.com>
2018-01-20 13:13:30 -0600

    twerkin on it

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python Week7-Homework.py
---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = 'FYWioG9cDv4XlhkB7XthOVX93'
+consumer_secret = 'jid4XOxgTL9oPSdcy3izAwX0DhZjXleci1U9Scq54zGYCmBEz7'
+
+access_token = '929813808952020992-71ulnPnAQKm1a0e2geBz8YmfzbWYAtv'
+access_token_secret = 'lwkL1ReMRrD6v0mvCOLf0v04uTUqR7YJnxYuHm9puEjqd'
